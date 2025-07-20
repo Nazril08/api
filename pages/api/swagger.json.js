70b8da95ea01ca411eba4ac395d9af7785f900cb
Nazril <bintangpratma088@gmail.com>
2025-07-20 09:28:49 +0700

    s

--- pages/api/swagger.json.js
+++ pages/api/swagger.json.js
@@ -1,18 +1,23 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 
-const swaggerOptions = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Yeyo Rest API',
-        version: '1.0.0',
-      },
-    },
-    apis: ['./pages/api/**/*.js'], // Important: point to the API routes
-  };
-
-const spec = swaggerJsdoc(swaggerOptions);
-
 export default function handler(req, res) {
+  const swaggerOptions = {
+      definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'Yeyo Rest API',
+          version: '1.0.0',
+        },
+        servers: [
+            {
+                // Infer server URL from request headers
+                url: `${req.headers['x-forwarded-proto'] || 'http'}://${req.headers.host}`
+            }
+        ]
+      },
+      apis: ['./pages/api/**/*.js'],
+    };
+
+  const spec = swaggerJsdoc(swaggerOptions);
   res.status(200).json(spec);
 } 
